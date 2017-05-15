60638e1f28079ed79ede8a971b9cd9474290bbb0
Bosîi Alexandru <aimnoemail@gmail.com>
2017-05-15 08:48:56 +0300

    als save model

--- src/main/java/com/eter/spark/app/als/ALS.java
+++ src/main/java/com/eter/spark/app/als/ALS.java
@@ -1,7 +1,5 @@
 package com.eter.spark.app.als;
 
-import org.apache.hadoop.fs.Hdfs;
-import org.apache.hadoop.fs.LocalFileSystem;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.mllib.recommendation.MatrixFactorizationModel;
@@ -12,13 +10,15 @@ import org.apache.spark.sql.SparkSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+
 /**
  * Created by abosii on 4/18/2017.
  */
 public class ALS {
     private static final Logger log = LoggerFactory.getLogger("ALS");
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         if (args.length < 1) {
             log.error("Can't find argument for model output");
@@ -39,33 +39,32 @@ public class ALS {
                         "/usr/local/hadoop/share/mapreduce/lib/*:" +
                         "/usr/local/hadoop/share/hadoop/mapreduce/*:" +
                         "/usr/local/hadoop/share/hadoop/common/*:" +
-                        "/usr/local/hadoop/share/hadoop/hdfs/*" +
+                        "/usr/local/hadoop/share/hadoop/hdfs/*:" +
                         "/usr//local/hadoop/etc/hadoop:" +
                         "/usr/local/hadoop/share/hadoop/common/lib/*:" +
                         "/usr/local/hadoop/share/hadoop/common/*:" +
                         "/usr/local/hive/lib/*:")
-                .config("fs.AbstractFileSystem.hdfs.impl", Hdfs.class.getName())
-                .config("fs.file.impl", LocalFileSystem.class.getName())
                 .enableHiveSupport()
                 .getOrCreate();
 
-        Dataset<Row> dataset = session.sql("SELECT * FROM productsrating");
+        Dataset<Row> dataset = session.sql("SELECT customerid, productid, rating FROM productsrating");
         MatrixFactorizationModel alsModel = org.apache.spark.mllib.recommendation.ALS
                 .train(transformToRDD(dataset).rdd(), 10, 10, 0.01);
-        System.out.println(alsModel.recommendProducts(123, 1)[0]);
+//        Pipeline pipeline = new Pipeline();
+//        pipeline.setStages(new PipelineStage[] {new org.apache.spark.ml.recommendation.ALS()});
+//        PipelineModel alsModel = pipeline.fit(dataset);
+//        System.out.println(alsModel.recommendProducts(123, 1)[0]);
         alsModel.save(session.sparkContext(), output);
     }
 
     public static JavaRDD<Rating> transformToRDD(Dataset<Row> dataset) {
-        return dataset.javaRDD().map(new Function<Row, Rating>() {
-            public Rating call(Row value) {
-                int userIdIndex = value.fieldIndex("customerid");
-                int productIdIndex = value.fieldIndex("productid");
-                int ratingIndex = value.fieldIndex("rating");
-                return new Rating((int) value.getLong(userIdIndex),
-                        (int) value.getLong(productIdIndex),
-                        value.getDouble(ratingIndex));
-            }
+        return dataset.javaRDD().map((Function<Row, Rating>) value -> {
+            int userIdIndex = value.fieldIndex("customerid");
+            int productIdIndex = value.fieldIndex("productid");
+            int ratingIndex = value.fieldIndex("rating");
+            return new Rating((int) value.getLong(userIdIndex),
+                    (int) value.getLong(productIdIndex),
+                    value.getDouble(ratingIndex));
         }).cache();
     }
 }
